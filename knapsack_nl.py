57e3592a18ede626bf236d462cbd0eb0c80d4139
Sara Jamous <129776424+sarajamous@users.noreply.github.com>
2025-01-30 23:33:58 +0000

    Changed the tabs to 4 spaces

--- knapsack_nl.py
+++ knapsack_nl.py
@@ -17,93 +17,92 @@
 
 
 def set_sampler():
-	"""Return a sampler"""
-
-	## TODO: Import the package and add sampler here
+    """Return a sampler"""
+    ## TODO: Import the package and add sampler here
     
 
-	return sampler
+    return sampler
 
 def build_nl(values, weights, capacity, max_items_allowed):
-	""" Build the NL model for our problem
+    """ Build the NL model for our problem
 
-	Requirements:
-		Objective: 
-			- Maximize the total value of the container
+    Requirements:
+        Objective: 
+            - Maximize the total value of the container
             
-		Constraints:
-			- Total weight of the shipped container should not exceed the capacity weight
-			- At most max_items_allowed can be included in the container
+        Constraints:
+            - Total weight of the shipped container should not exceed the capacity weight
+            - At most max_items_allowed can be included in the container
             
-	Args:
-		values (list):
-			A 1 D array-like (row vector) listing the value of each item
+    Args:
+        values (list):
+            A 1 D array-like (row vector) listing the value of each item
             
-		weights (list):
-			A 1 D array-like (row vector) listing the weight of each item
+        weights (list):
+            A 1 D array-like (row vector) listing the weight of each item
             
-		capacity (int):
-			Maximum weight the container can hold
+        capacity (int):
+            Maximum weight the container can hold
                
-		max_items_allowed (int):
-			The maximum number of items that can be included in the container
+        max_items_allowed (int):
+            The maximum number of items that can be included in the container
                
-	Returns:
-		A model encoding the knapsack problem
+    Returns:
+        A model encoding the knapsack problem
     """
    
-	## TODO: Import the package and construct the model
+    ## TODO: Import the package and construct the model
 	
 
-	# Create the binary variables 
-	nbr_items = len(weights)
-	x = model.binary(nbr_items)
+    # Create the binary variables 
+    nbr_items = len(weights)
+    x = model.binary(nbr_items)
 
-	# Add constants 
-	values = model.constant(values)
-	weights = model.constant(weights)
-	capacity = model.constant(capacity)
-	max_items_allowed = model.constant(max_items_allowed)
+    # Add constants 
+    values = model.constant(values)
+    weights = model.constant(weights)
+    capacity = model.constant(capacity)
+    max_items_allowed = model.constant(max_items_allowed)
 
-	## TODO: Set the objective mentioned in the README
+    ## TODO: Set the objective mentioned in the README
 	
 
-	## TODO: Add constraints to reflect the restrictions in the README
+    ## TODO: Add constraints to reflect the restrictions in the README
 	
 	
         
-	return model
+    return model
 
 def solve_problem(model, sampler):
-	"""Run the provided NL object on the designated sampler"""
+    """Run the provided NL object on the designated sampler"""
 
-	# Initialize the NL solver
-	sampler = set_sampler()
+    # Initialize the NL solver
+    sampler = set_sampler()
 
-	# Solve the problem using the NL solver
-	sampleset = sampler.sample(model, label='Knapsack using NL solver')
+    # Solve the problem using the NL solver
+    sampleset = sampler.sample(model, label='Knapsack using NL solver')
 
-	return sampleset
+    return sampleset
 
 ## ------- Main program -------
 if __name__ == "__main__":
 	
-	# Add constants 
-	values = [34, 25, 78, 21, 64]
-	weights = [3, 5, 9, 4, 2]
-	W = 10
-	max_items_allowed = 2
+    # Add constants 
+    values = [34, 25, 78, 21, 64]
+    weights = [3, 5, 9, 4, 2]
+    W = 10
+    max_items_allowed = 2
 
-	## Construct the model
-	model = build_nl(values, weights, W, max_items_allowed)
+    ## Construct the model
+    model = build_nl(values, weights, W, max_items_allowed)
 	
-	## ------- Run on the QPU -------
-	sampler = set_sampler()
+    ## ------- Run on the QPU -------
+    sampler = set_sampler()
 
-	sampleset = solve_problem(model, sampler)
+    sampleset = solve_problem(model, sampler)
 
-	# Obtain results 
-	with model.lock():
-		current_state = 0
-		print(f"For state {current_state}, {list(sym.state(current_state) for sym in model.iter_decisions())} results in objective {model.objective.state(current_state)}")
-
+    # Obtain results 
+    with model.lock():
+        current_state = 0
+        print(f"For state {current_state}, {list(sym.state(current_state) for sym in model.iter_decisions())} results in objective {model.objective.state(current_state)}")
+git branch -a -v
